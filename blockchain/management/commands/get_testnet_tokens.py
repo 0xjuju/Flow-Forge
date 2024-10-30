510bd667af9fbf11cf63f6f4d80c5a8b096b682a
0xjuju <jermol.jupiter@gmail.com>
2024-10-30 11:34:07 -0400

    Create command to request testnet tokens

--- blockchain/management/commands/get_testnet_tokens.py
+++ blockchain/management/commands/get_testnet_tokens.py
@@ -1,4 +1,6 @@
 
+from blockchain.web3_api import Blockchain
+import decouple
 from django.core.management.base import BaseCommand
 
 
@@ -6,7 +8,8 @@ class Command(BaseCommand):
     help = "Request testnet tokens for specified chain"
 
     def handle(self, *args, **options):
-        pass
-
-
+        chain = input("Chain: ")
+        network = input("Network: ")
+        blockchain = Blockchain(chain, network)
+        blockchain.request_testnet_tokens(decouple.config("WALLET_ADDRESS"))
 
