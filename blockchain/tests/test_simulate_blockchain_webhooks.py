0719f6631d286b14459f2a8b6776f0b20ff1bb8d
0xjuju <jermol.jupiter@gmail.com>
2024-11-02 15:54:25 -0400

    create setup vars for simulation

--- blockchain/tests/test_simulate_blockchain_webhooks.py
+++ blockchain/tests/test_simulate_blockchain_webhooks.py
@@ -1,12 +1,18 @@
 
+from blockchain.web3_api import Blockchain
+import decouple
 from django.test import TestCase
 
 
 class BlockchainSimulationTests(TestCase):
 
     def setUp(self):
-        pass
+        self.test_token_contract = decouple.config("TEST_TOKEN_CONTRACT")
+        self.sepolia = Blockchain("ethereum", "sepolia")
+        self.base_address = self.sepolia.ACCOUNT
+        self.user_wallet = "0xbA1f6d33bc01A90020fE41a50e52EDD26B018068"
 
     def test_simulate_webhook(self):
-        pass
+
+        self.sepolia.transfer_tokens(self.test_token_contract, self.user_wallet, 100)
 
